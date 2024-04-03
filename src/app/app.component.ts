916bc4fe5337614e1572b4facbfdbe25757654c0
Sam Sheets <sam.sheets94@gmail.com>
2024-04-03 15:09:04 -0600

    OnInit func

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,25 @@
 // Imports
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 // Decorators 
 @Component({
-  selector: 'app-root', //tag name
+  selector: 'app-root', //tag names
   standalone: true,
   imports: [RouterOutlet],
-  templateUrl: './app.component.html',
+  templateUrl: './app.component.html', // could also be inline
   styleUrl: './app.component.css'
 })
 
 // Class
-export class AppComponent {
-  title = 'leaning-angular';
+export class AppComponent implements OnInit{
+  title: string | undefined;
+
+  constructor () {
+
+  }
+
+  ngOnInit(): void {
+    this.title = 'leaning-angular!'
+  }
 }
