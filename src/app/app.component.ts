23da95e66797a9022dc8ceefdb55e0d548446998
Sam Sheets <sam.sheets94@gmail.com>
2024-04-03 15:00:03 -0600

    explore component file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,17 @@
+// Imports
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+// Decorators 
 @Component({
-  selector: 'app-root',
+  selector: 'app-root', //tag name
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
+
+// Class
 export class AppComponent {
   title = 'leaning-angular';
 }
